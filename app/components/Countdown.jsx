24d8c1aca0108c11e6e597824c455a045d166830
Mark Patton <mark@emp-technical.com>
2016-08-02 21:03:57 +0100

    Add in timer and countdown routes with active link styling

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Countdown = () => {
+  return (
+    <p>Countdown rendered!</p>
+  );
+}
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Timer = () => {
+  return (
+    <p>Timer rendered!</p>
+  );
+}
+
+module.exports = Timer;
